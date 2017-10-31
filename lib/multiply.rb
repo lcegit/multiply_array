03e3e14cd70c43ae8d5be1550b079377e979d71d
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-10-31 21:30:12 +0100

    reworked multiplyer method

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,13 +1,9 @@
-  # class Multiply
-
-  def by_two(array, timestwo)
-    result = [] # new array built here
-    m = 1
-    array.each_index do |n|
-      result[n] = array[n] * (m + (timestwo)) #stores the result
-    end
-    return result
-  end
-
+# class Multiply
 array = [1, 2, 3, 4]
-new_array = by_two(array, 1)
+  def product(array, num)
+    result = []
+    array.each do |n|
+      result.push(n * num)
+    end
+    result
+  end
--- spec/multiply_spec.rb
+++ spec/multiply_spec.rb
@@ -1,8 +1,8 @@
 require './lib/multiply.rb'
 
-describe '.new_array' do
+describe 'product' do
 
-  it 'multiplies items in array by 2' do
-      expect([1, 2, 3, 4]).to match_array [2, 4, 6, 8]
+  it 'multiplies intigers in array by a number' do
   end
+
 end
