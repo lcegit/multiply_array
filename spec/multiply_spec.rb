f6ce1cd530bc6ea9969220cd2b85f3b55962f58d
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-10-31 21:37:23 +0100

    finalize test

--- spec/multiply_spec.rb
+++ spec/multiply_spec.rb
@@ -3,6 +3,20 @@ require './lib/multiply.rb'
 describe 'product' do
 
   it 'multiplies intigers in array by a number' do
+    expect([1]).to include(be_odd)
   end
+
+  it 'multiplies intigers in array by a number' do
+    expect([2]).not_to include(be_odd)
+  end
+
+  it 'multiplies intigers in array by a number' do
+    expect([3]).to include(be_odd)
+  end
+
+  it 'multiplies intigers in array by a number' do
+    expect([4]).not_to include(be_odd)
+  end
+
 
 end
